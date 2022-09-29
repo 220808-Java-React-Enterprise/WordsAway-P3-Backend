0a74df7e1f8de5e60ceae5a5aa0357ce63163cd9
del4235 <110851058+del4235@users.noreply.github.com>
2022-09-29 11:43:41 -0400

    Pulled from main corrected finduser to use token instead of params

--- src/main/java/com/revature/wordsaway/controllers/UserController.java
+++ src/main/java/com/revature/wordsaway/controllers/UserController.java
@@ -1,12 +1,15 @@
 package com.revature.wordsaway.controllers;
-import com.revature.wordsaway.dtos.requests.NewUserRequest;
 import com.revature.wordsaway.dtos.responses.FindUserResponse;
+import com.revature.wordsaway.entities.User;
+import com.revature.wordsaway.services.TokenService;
 import com.revature.wordsaway.services.UserService;
 import com.revature.wordsaway.utils.customExceptions.NetworkException;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 @RestController
 @RequestMapping
@@ -14,10 +17,11 @@ public class UserController {
 
     @CrossOrigin
     @GetMapping(value = "/findUser", produces = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody FindUserResponse finduser(@RequestParam(required = false) String username, HttpServletResponse req, HttpServletResponse resp) {
+    public @ResponseBody FindUserResponse finduser(HttpServletRequest req, HttpServletResponse resp) {
         try {
+            User user = TokenService.extractRequesterDetails(req);
             resp.setStatus(200);
-            return UserService.getFriendByUsername(username);
+            return UserService.getFriendByUsername(user.getUsername());
         }catch (NetworkException e){
             resp.setStatus(e.getStatusCode());
             System.out.println(e.getMessage());
@@ -25,4 +29,6 @@ public class UserController {
             return null;
         }
     }
+
+
 }
