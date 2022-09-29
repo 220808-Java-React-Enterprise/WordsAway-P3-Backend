b850c34bec3d1773cdad92a02bebbe49eaf6dafa
Koukaakiva <koukaakiva@gmail.com>
2022-09-29 14:24:01 -0400

    Merged.

--- src/main/java/com/revature/wordsaway/controllers/UserController.java
+++ src/main/java/com/revature/wordsaway/controllers/UserController.java
@@ -1,4 +1,5 @@
 package com.revature.wordsaway.controllers;
+
 import com.revature.wordsaway.dtos.responses.FindUserResponse;
 import com.revature.wordsaway.models.entities.Board;
 import com.revature.wordsaway.models.entities.User;
@@ -8,7 +9,6 @@ import com.revature.wordsaway.services.UserService;
 import com.revature.wordsaway.utils.customExceptions.NetworkException;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
@@ -20,15 +20,14 @@ public class UserController {
 
     @CrossOrigin
     @GetMapping(value = "/findUser", produces = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody FindUserResponse findUser(@RequestParam String friend_name, HttpServletRequest req, HttpServletResponse resp) {
+    public @ResponseBody FindUserResponse findUser(@RequestParam String username, HttpServletRequest req, HttpServletResponse resp) {
         try {
             User user = TokenService.extractRequesterDetails(req);
             resp.setStatus(200);
-            return UserService.getFriendByUsername(friend_name);
+            return UserService.getFriendByUsername(username);
         }catch (NetworkException e){
             resp.setStatus(e.getStatusCode());
             System.out.println(e.getMessage());
-            //return e.getMessage();
             return null;
         }
     }
