c0ec76ba6082c4233df95beb9b636fe301f5e995
Robert James <rjrobertjames91@gmail.com>
2022-09-30 09:16:19 -0400

    Added local list of valid words, bot was adjusted to use that list

--- src/main/java/com/revature/wordsaway/MainDriver.java
+++ src/main/java/com/revature/wordsaway/MainDriver.java
@@ -15,37 +15,6 @@ import static com.revature.wordsaway.utils.Constants.*;
 @SpringBootApplication
 public class MainDriver {
     public static void main(String[] args) {
-        //SpringApplication.run(MainDriver.class, args);
-
-        char[] tray = new char[7];
-        BoardService.getNewTray(tray);
-        char[] letters = new char[BOARD_SIZE*BOARD_SIZE];
-        char[] worms = new char[BOARD_SIZE*BOARD_SIZE];
-        Arrays.fill(letters, '.');
-        Arrays.fill(worms, '.');
-
-        User user = new User("username", "password", "salt", "email", 0, 500, 0, 0, true, new HashSet<>());
-        Board board = new Board(UUID.randomUUID(), user, tray, 0, worms, letters, UUID.randomUUID(), true);
-
-        System.out.println(START_POINT_BY_WORD_LENGTH);
-
-        long start = System.currentTimeMillis();
-
-        for (int i = 0; i < 50; i++){
-            board = AIService.start(System.currentTimeMillis(), board);
-            BoardService.getNewTray(tray);
-            board.setTray(tray);
-        }
-
-        System.out.println((float)(System.currentTimeMillis() - start) / 1000);
-        System.out.println(AIService.temp);
-
-        char[] newLetters = board.getLetters();
-        int counter = 0;
-        for (int i = 0; i < BOARD_SIZE; i++) {
-            for (int j = 0; j < BOARD_SIZE; j++)
-                System.out.print(newLetters[counter++] + ", ");
-            System.out.println();
-        }
+        SpringApplication.run(MainDriver.class, args);
     }
 }
