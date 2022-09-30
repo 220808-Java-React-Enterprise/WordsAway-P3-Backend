ff57791dea371dec1109d3d09bac17a2a5f33887
Robert James <rjrobertjames91@gmail.com>
2022-09-30 09:17:45 -0400

    Added local list of valid words, bot was adjusted to use that list

--- src/main/java/com/revature/wordsaway/MainDriver.java
+++ src/main/java/com/revature/wordsaway/MainDriver.java
@@ -1,16 +1,7 @@
 package com.revature.wordsaway;
 
-import com.revature.wordsaway.entities.Board;
-import com.revature.wordsaway.entities.User;
-import com.revature.wordsaway.services.AIService;
-import com.revature.wordsaway.services.AnagramService;
-import com.revature.wordsaway.services.BoardService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.*;
-
-import static com.revature.wordsaway.utils.Constants.*;
 
 @SpringBootApplication
 public class MainDriver {
