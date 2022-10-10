362d57cf80c904fdd03f13429457cee5eaef316a
John <nguyen8585@gmail.com>
2022-10-10 09:47:41 -0700

    update on ChatServiceTest

--- src/test/java/com/revature/wordsaway/services/ChatServiceTest.java
+++ src/test/java/com/revature/wordsaway/services/ChatServiceTest.java
@@ -1,6 +1,8 @@
 package com.revature.wordsaway.services;
 
 import com.revature.wordsaway.dtos.requests.NewUserRequest;
+import com.revature.wordsaway.models.entities.Chat;
+import com.revature.wordsaway.models.entities.Message;
 import com.revature.wordsaway.models.entities.User;
 import com.revature.wordsaway.repositories.BoardRepository;
 import com.revature.wordsaway.repositories.ChatRepository;
@@ -8,6 +10,11 @@ import com.revature.wordsaway.repositories.MessageRepository;
 import com.revature.wordsaway.repositories.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -45,10 +52,18 @@ class ChatServiceTest {
 
     @Test
     void register() {
+        Chat c = new Chat(UUID.randomUUID(), new ArrayList<Message>(), new HashSet<>());
+        when(mockChatRepository.save(c)).thenReturn(c);
+        chatService.register(c);
+        verify(mockChatRepository, times(1)).save(c);
     }
 
     @Test
     void addMessage() {
+        Chat c = new Chat(UUID.randomUUID(), new ArrayList<Message>(), new HashSet<>());
+        Message message = new Message();
+        when(mockChatRepository.save(c)).thenReturn(c);
+        when(mockMessageRepository.save(c)).thenReturn(c);
     }
 
     @Test
